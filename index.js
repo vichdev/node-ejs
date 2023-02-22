1aa2ce7bcb2840366a75a72a8ed86c0ec2516b47
victor.nunes <victor.nunes@avonale.com.br>
2023-02-22 16:37:12 -0300

    agora vAI

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-// const axios = require("axios");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.set("view engine", "ejs");
-// app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
 
 app.get("/", async (req, res) => {
   if (req.query.id) {
