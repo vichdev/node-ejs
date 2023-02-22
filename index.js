484ce716acc9a2b562fcc5236803e030a1fc7cc9
victor.nunes <victor.nunes@avonale.com.br>
2023-02-22 17:04:28 -0300

    thfd

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ app.set("view engine", "ejs");
 app.set("views", "./src/views");
 
 app.get("/", async (req, res) => {
-  const resp = await axios.get(
-    `https://qe0vikexaj.execute-api.us-east-1.amazonaws.com/Prod/api/Veiculo/ObterVeiculoPublico?identificadorPublico=MQ7AM3DICL1K`
-  );
-  console.log(resp.data.data);
-  const veiculo = resp.data.data;
-  const pokeBody = {
-    name: `${veiculo.fabricante} ${veiculo.modelo} ${veiculo.versao}`,
-    description: `informação sobre o veiculo de marca: ${veiculo.fabricante}`,
-  };
-  res.render("pokemon", { pokemon: pokeBody });
+  if (req.query.id) {
+    const id = req.query.id;
+    const resp = await axios.get(
+      `https://qe0vikexaj.execute-api.us-east-1.amazonaws.com/Prod/api/Veiculo/ObterVeiculoPublico?identificadorPublico=${id}K`
+    );
+    const veiculo = resp.data.data;
+    const pokeBody = {
+      name: `${veiculo.fabricante} ${veiculo.modelo} ${veiculo.versao}`,
+      description: `informação sobre o veiculo de marca: ${veiculo.fabricante}`,
+    };
+    res.render("pokemon", { pokemon: pokeBody });
+  }
 });
 
 app.listen(port, () => {
