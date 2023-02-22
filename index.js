3c72fc50b2484fe605e9d291c1b50bd8ef26fbea
victor.nunes <victor.nunes@avonale.com.br>
2023-02-22 16:27:52 -0300

    teste

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require("express");
-const axios = require("axios");
+// const axios = require("axios");
 
 const app = express();
 const port = 3000;
 
-app.set("view engine", "ejs");
-app.set("views", "./src/views");
+// app.set("view engine", "ejs");
+// app.set("views", "./src/views");
 
 app.get("/", async (req, res) => {
-  if (req.query.id) {
-    const id = req.query.id;
-    const resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const pokemon = resp.data;
-    const pokeBody = {
-      img: pokemon.sprites.front_default,
-      name: pokemon.name,
-    };
-    res.render("pokemon", { pokemon: pokeBody });
-  }
+  res.send("funcionou");
+  // if (req.query.id) {
+  //   const id = req.query.id;
+  //   const resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  //   const pokemon = resp.data;
+  //   const pokeBody = {
+  //     img: pokemon.sprites.front_default,
+  //     name: pokemon.name,
+  //   };
+  //   res.render("pokemon", { pokemon: pokeBody });
+  // }
 });
 
 app.listen(port, () => {
